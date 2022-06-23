a04d123c81a02b3bd6186bce51802495c3e1d83a
rmartinc <rmartinc@redhat.com>
2022-06-23 10:01:26 +0200

    8288895: LdapContext doesn't honor set referrals limit

--- src/java.naming/share/classes/com/sun/jndi/ldap/AbstractLdapNamingEnumeration.java
+++ src/java.naming/share/classes/com/sun/jndi/ldap/AbstractLdapNamingEnumeration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -310,10 +310,8 @@ abstract class AbstractLdapNamingEnumeration<T extends NameClassPair>
      */
     protected final boolean hasMoreReferrals() throws NamingException {
 
-        if ((refEx != null) &&
-            (refEx.hasMoreReferrals() ||
-             refEx.hasMoreReferralExceptions()
-                && !(errEx instanceof LimitExceededException))) {
+        if ((refEx != null) && !(errEx instanceof LimitExceededException) &&
+            (refEx.hasMoreReferrals() || refEx.hasMoreReferralExceptions())) {
 
             if (homeCtx.handleReferrals == LdapClient.LDAP_REF_THROW) {
                 throw (NamingException)(refEx.fillInStackTrace());
@@ -333,8 +331,11 @@ abstract class AbstractLdapNamingEnumeration<T extends NameClassPair>
 
                 } catch (LdapReferralException re) {
 
-                    // record a previous exception
-                    if (errEx == null) {
+                    // record a previous exception and quit if any limit is reached
+                    if (re.getNamingException() instanceof LimitExceededException) {
+                        errEx = re.getNamingException();
+                        break;
+                    } else if (errEx == null) {
                         errEx = re.getNamingException();
                     }
                     refEx = re;
@@ -381,6 +382,10 @@ abstract class AbstractLdapNamingEnumeration<T extends NameClassPair>
         entries = ne.entries;
         refEx = ne.refEx;
         listArg = ne.listArg;
+        // record a previous exception and quit if any limit is reached
+        if (errEx == null || ne.errEx instanceof LimitExceededException) {
+            errEx = ne.errEx;
+        }
     }
 
     @SuppressWarnings("removal")
